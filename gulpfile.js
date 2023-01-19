106bc9ca24369a0ad7abb4a30737d6c28c7ef941
Martin del Pino <dellpinos7@gmail.com>
2023-01-19 11:54:32 -0300

    finalizando proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require("gulp"); // extraigo las funcionalidades de la dependencia gulp
+const { src, dest, watch, parallel } = require("gulp"); // extraigo las funcionalidades de la dependencia gulp
+
+// CSS
+
 const sass = require("gulp-sass")(require('sass')); // extraigo gulp-sass y con esta extraigo sass
 const plumber = require('gulp-plumber');
+
+// Imagenes
+const webp = require("gulp-webp");
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin"); //la variable puede tener cualquier nombre
+const avif = require("gulp-avif");
 
 function css (done) { //defino la funcion/tarea
 
@@ -12,14 +21,66 @@ function css (done) { //defino la funcion/tarea
     done(); // callback
 }
 
+function imagenes (done) {
+
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache( imagemin( opciones) ) )
+    .pipe( dest('build/img'))
+
+    done();
+}
+
+function versionWebp (done) { //comprime imagenes a webp
+
+    const opciones = { // argumento de calidad de la compresion
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe ( webp(opciones) )
+    .pipe ( dest('build/img') );
+    
+    done();
+}
+function versionAvif (done) { //comprime imagenes a webp
+
+    const opciones = { // argumento de calidad de la compresion
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe ( avif(opciones) )
+    .pipe ( dest('build/img') );
+    
+    done();
+}
+
+function javascript(done) {
+    src('src/js/**/*.js')
+    .pipe ( dest( 'build/js'));
+
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
 
     done();
 }
 
 exports.css = css; // nombre de la tarea
-exports.dev = dev; // nombre de la tarea
+exports.imagenes;
+exports.versionWebp = versionWebp; //nombre de tarea
+exports.versionAvif = versionAvif;
+exports.javascript = javascript;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev); // nombre de la tarea
+
+
 
 
 // un pipe es una accion q se realiza despues de otra
